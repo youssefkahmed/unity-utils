e87c0e384c8bccdbfb8a6ba4405f8356b3047fb2
youssefkahmed <youssefkahmed@gmail.com>
2024-12-03 21:59:52 +0200

    Project setup tool

---
+++ Assets/_Project/Scripts/Editor/ProjectSetupTool.cs
@@ -0,0 +1,151 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using static System.IO.Path;
+using static UnityEditor.AssetDatabase;
+
+namespace Utils
+{
+    public static class Setup
+    {
+        [MenuItem("Utils/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+            Folders.CreateDefault("_Project", 
+                "Animations",
+                "Data",
+                "Fonts",
+                "Input",
+                "Materials",
+                "Models",
+                "ParticleEffects",
+                "Prefabs",
+                "Scenes",
+                "Scripts",
+                "SFX",
+                "Shaders",
+                "Textures");
+            
+            Refresh();
+        }
+
+        [MenuItem("Utils/Setup/Import Favorite Assets")]
+        public static void ImportMyFavoriteAssets()
+        {
+            Assets.ImportAsset("DOTween HOTween v2.unitypackage", "Demigiant/Editor ExtensionsAnimation");
+            Assets.ImportAsset("Gridbox Prototype Materials.unitypackage", "Ciathyza/Textures Materials");
+            Assets.ImportAsset("Mulligan Renamer.unitypackage", "Red Blue Games/Editor ExtensionsUtilities");
+        }
+
+        [MenuItem("Utils/Setup/Install Favorite Open Source Assets")]
+        public static void InstallOpenSource()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "git+https://github.com/KyleBanks/scene-ref-attribute",
+                "git+https://github.com/starikcetin/Eflatun.SceneReference.git#3.1.1"
+            });
+        }
+        
+        [MenuItem("Utils/Setup/Install Unity AI Navigation")]
+        public static void InstallUnityAINavigation()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.ai.navigation"
+            });
+        }
+
+        private static class Folders
+        {
+            public static void CreateDefault(string root, params string[] folders)
+            {
+                string fullPath = Combine(Application.dataPath, root);
+                if (!Directory.Exists(fullPath))
+                {
+                    Directory.CreateDirectory(fullPath);
+                }
+                
+                foreach (string folder in folders)
+                {
+                    CreateSubFolders(fullPath, folder);
+                }
+            }
+    
+            private static void CreateSubFolders(string rootPath, string folderHierarchy)
+            {
+                string[] folders = folderHierarchy.Split('/');
+                string currentPath = rootPath;
+                foreach (string folder in folders)
+                {
+                    currentPath = Combine(currentPath, folder);
+                    if (!Directory.Exists(currentPath))
+                    {
+                        Directory.CreateDirectory(currentPath);
+                    }
+                }
+            }
+        }
+
+        private static class Packages
+        {
+            private static AddRequest _request;
+            private static readonly Queue<string> PackagesToInstall = new();
+
+            public static void InstallPackages(IEnumerable<string> packages)
+            {
+                foreach (string package in packages)
+                {
+                    PackagesToInstall.Enqueue(package);
+                }
+
+                // Start the installation of the first package
+                if (PackagesToInstall.Count > 0)
+                {
+                    _request = Client.Add(PackagesToInstall.Dequeue());
+                    EditorApplication.update += Progress;
+                }
+            }
+
+            private static async void Progress()
+            {
+                if (!_request.IsCompleted)
+                {
+                    return;
+                }
+                
+                if (_request.Status == StatusCode.Success)
+                {
+                    Debug.Log($"Installed: {_request.Result.packageId}");
+                }
+                else if (_request.Status >= StatusCode.Failure)
+                {
+                    Debug.Log(_request.Error.message);
+                }
+
+                EditorApplication.update -= Progress;
+
+                // If there are more packages to install, start the next one
+                if (PackagesToInstall.Count > 0)
+                {
+                    // Add delay before next package install
+                    await Task.Delay(1000);
+                    _request = Client.Add(PackagesToInstall.Dequeue());
+                    EditorApplication.update += Progress;
+                }
+            }
+        }
+
+        private static class Assets
+        {
+            public static void ImportAsset(string asset, string subfolder, string rootFolder = "C:/Users/Youssef/AppData/Roaming/Unity/Asset Store-5.x")
+            {
+                ImportPackage(Combine(rootFolder, subfolder, asset), false);
+            }
+        }
+    }
+}
